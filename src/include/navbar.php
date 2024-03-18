73019f015e348b5f5debdcb231eb8a6de6ce1958
chetsada <j.a.y.jsd3@gmail.com>
2024-03-18 21:18:56 +0700

    📦 NEW: navbar

---
+++ src/include/navbar.php
@@ -0,0 +1,31 @@
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+
+<nav class="navbar navbar-light shadow-sm" style="background-color: #B8D8BA;">
+    <div class="container d-flex justify-content-between align-items-center">
+        <div>
+            <a class="navbar-brand" href="index.php">
+                <img src="path/to/your/logo/image.png" width="30" height="30" class="d-inline-block align-top" alt="">
+                LOGO
+            </a>
+        </div>
+
+        <div class="text-center">
+            <form class="form-inline">
+                <div class="input-group">
+                    <input class="form-control" type="search" placeholder="Search..." aria-label="Search"
+                        style="width: 30vw; border-radius: 30px;">
+                    <button type="submit" class="btn"><i class="bi bi-search" style="font-size: 1rem;"></i></button>
+                </div>
+            </form>
+        </div>
+
+
+        <div>
+            <a href="#" class="btn" style="color: #000000;">
+                <i class="bi bi-cart" style="font-size: 1.5rem;"></i>
+            </a>
+            <a class="btn btn-sm fw-bold" href="register.php" role="button"
+                style="background-color: #EF959D; border-color: #EF959D; width: 100px; border-radius: 50px;">Sign In</a>
+        </div>
+    </div>
+</nav>
