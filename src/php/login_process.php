5d642fb494bfc2cf72154cfb1d13180f2a800b8b
MaxVerstappen69 <163567312+MaxVerstappen69@users.noreply.github.com>
2024-04-06 21:18:45 +0700

    👌 IMPROVE: employee nnow can login

--- src/php/login_process.php
+++ src/php/login_process.php
@@ -7,24 +7,27 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $password = md5($_POST['password']);
 
-    // คิวรี่ฐานข้อมูลเพื่อตรวจสอบข้อมูลผู้ใช้
-    $query = "SELECT * FROM customer WHERE email='$email' AND password='$password'";
-    $result = mysqli_query($conn, $query);
+    // คิวรี่ฐานข้อมูลเพื่อตรวจสอบข้อมูลผู้ใช้ในตาราง customer
+    $query_customer = "SELECT * FROM customer WHERE email='$email' or username ='$email' AND password='$password'";
+    $result_customer = mysqli_query($conn, $query_customer);
 
+    // คิวรี่ฐานข้อมูลเพื่อตรวจสอบข้อมูลผู้ใช้ในตาราง employee
+    $query_employee = "SELECT * FROM employee WHERE em_email='$email' or em_username ='$email' AND em_password='$password'";
+    $result_employee = mysqli_query($conn, $query_employee);
 
-    // ถ้าพบข้อมูลผู้ใช้ในฐานข้อมูล
-    if (mysqli_num_rows($result) == 1) {
-
-        $row = mysqli_fetch_array($result);
-
+    // ถ้าพบข้อมูลผู้ใช้ในตาราง customer
+    if (mysqli_num_rows($result_customer) == 1) {
+        $row = mysqli_fetch_array($result_customer);
         $_SESSION['login_user'] = $email; // เก็บค่าอีเมล์ผู้ใช้ใน session
         $_SESSION['user_role'] = $row['user_role'];
-
-        if ($_SESSION['user_role'] === 'admin' || $_SESSION['user_role'] === 'user') {
-            header("location: index.php");
-            
-        }
-        
+        header("location: index.php");
+    }
+    // ถ้าพบข้อมูลผู้ใช้ในตาราง employee
+    elseif (mysqli_num_rows($result_employee) == 1) {
+        $row = mysqli_fetch_array($result_employee);
+        $_SESSION['login_user'] = $email; // เก็บค่าอีเมล์ผู้ใช้ใน session
+        $_SESSION['user_role'] = $row['user_role'];
+        header("location: index.php"); // ปล. เปลี่ยนไปยังหน้า dashboard ของ employee
     } else {
         $_SESSION['error'] = 'รหัสผ่านไม่ถูกต้อง';
         header('location: login.php');
@@ -32,10 +35,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 
     // ตรวจสอบว่ากล่อง "จดจำฉัน" ถูกเลือกหรือไม่
-    if (isset ($_POST['remember_me'])) {
+    if (isset($_POST['remember_me'])) {
         // ทำสิ่งที่คุณต้องการเมื่อกล่อง "จดจำฉัน" ถูกเลือก
         // เช่น บันทึกข้อมูลลงในคุกกี้
     }
 }
+
 
 ?>
